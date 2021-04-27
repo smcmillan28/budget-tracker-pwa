e59cf8633b65298941db7d75e2396477f7738341
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-04-26 20:37:59 -0400

    indexedDB allowing user to add transactions to DB

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -2,6 +2,7 @@ const transName = document.getElementById("t-name");
 const transAmount = document.getElementById("t-amount");
 const addButton = document.getElementById("add-btn");
 const subButton = document.getElementById("sub-btn");
+const buttons = document.querySelector("button");
 
 const request = window.indexedDB.open("budget", 1);
 
@@ -9,21 +10,33 @@ const request = window.indexedDB.open("budget", 1);
 request.onupgradeneeded = event => {
     const db = event.target.result;
 
-    const budgetStore = db.createObjectStore("budget", {keyPath: "name"});
+    const budgetStore = db.createObjectStore("budget", { keyPath: "name" });
     budgetStore.createIndex("transactionIndex", "amount");
 }
 
-request.onsuccess = () => {
+addButton.addEventListener("click", request.onsuccess = () => {
     const db = request.result;
     const transaction = db.transaction(["budget"], "readwrite");
     const budgetStore = transaction.objectStore("budget");
     const transactionIndex = budgetStore.index("transactionIndex");
 
     budgetStore.add({
-        name: "Rent",
-        amount: 1200
+        name: transName.value,
+        amount: Math.abs(transAmount.value)
     });
-}
+});
+
+subButton.addEventListener("click", request.onsuccess = () => {
+    const db = request.result;
+    const transaction = db.transaction(["budget"], "readwrite");
+    const budgetStore = transaction.objectStore("budget");
+    const transactionIndex = budgetStore.index("transactionIndex");
+
+    budgetStore.add({
+        name: transName.value,
+        amount: -Math.abs(transAmount.value)
+    });
+});
 
 // Writing async function that will create the database then allow us to write/pull from it
 // addButton.addEventListener("click", function(event) {
